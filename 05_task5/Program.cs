ac7b78c3e771a22ce9d94cce3fef7574be2f6817
Ayfer Şahin <ayfers547@gmail.com>
2024-10-12 23:01:20 +0300

    added making shapes with *

---
+++ 05_task5/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region making shapes with *
+            //for(int i = 0;i<=10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            //for (int i = 0; i<=10;  i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+
+            //for (int i = 0;i<5; i++)
+            //{
+            //    for(int j = 0;j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 5;i>0;i--)
+            //{
+            //    for(int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            /*
+            for (int i = 0;i<=5; i++)
+            {
+                for(int j = 0;j<=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for(int i = 4; i>=0; i--)
+            {
+                for(int j=0;j<=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }*/
+            /*
+            int n = 5;
+            for(int i = 1; i<=n;  i++)
+            {
+                for(int j = n - i; j>0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int y = 1; y <=2*i-1; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+           
+            for(int k = n-1; k>=1; k--)
+            {
+                for(int l= n - k; l>0; l--)
+                {
+                    Console.Write(" ");
+                }
+                for(int t=1; t<=2*k-1; t++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }*/
+            #endregion
+            Console.Read();
+        }
+    }
+}
