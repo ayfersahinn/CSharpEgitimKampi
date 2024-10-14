e4659d8f34ae01989d42c1cf223bd65469b2953a
Ayfer Şahin <ayfers547@gmail.com>
2024-10-14 22:55:31 +0300

    added array examples

---
+++ 06_task6/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region arrays
+            //string[] colors = new string[4];
+            //colors[0] = "red";
+            //colors[1] = "yellow";
+            //colors[2] = "blue";
+            //colors[3] = "green";
+
+            //Console.WriteLine(colors[2]);
+
+            //int[] numbers = new int[5];
+            //numbers[0] = 15;
+            //numbers[1] = 58;
+            //numbers[4] = 102;
+
+            //Console.WriteLine(numbers[4]);
+            //Console.WriteLine(numbers[3]);
+
+
+            //string[] cities = { "istanbul", "ankara", "izmir" };
+            //Console.WriteLine(cities[1]);
+            #endregion
+
+            #region arrays and for loop
+            //string[] colors = { "white", "black", "pink", "purple" };
+            //for (int i = 0; i < colors.Length; i++)
+            //{
+            //    Console.WriteLine(colors[i]);
+            //}
+
+            //int[] numbers = { 1, 2, 45, 895, 233, 48, 26, 34, 77 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2==1)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+            //}
+
+            //int[] nums = { 1, 2, 78, 259, 756, 4263, 55, 23 };
+            //int max = nums[0];
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] > max)
+            //    {
+            //        max = nums[i];
+            //    }
+
+            //}
+            //Console.WriteLine(max);
+
+            //int[] nums = { 1, 2, 7, 8, 25, 5, 203, 44, 9 };
+            //Array.Sort(nums);
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.WriteLine(nums[i]);
+            //}
+
+            //int[] nums = { 1, 2, 7, 8, 25, 5, 203, 44, 9 };
+            //Array.Reverse(nums);
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.WriteLine(nums[i]);
+            //}
+
+            //string[] names = { "ali", "can", "kerem", "ayça" };
+            //int index = Array.IndexOf(names,"can");
+            //Console.WriteLine(index);
+
+            //int[] nums = { 1, 2, 7, 8, 25, 5, 203, 44, 9 };
+            //Console.WriteLine("max eleman: "+nums.Max()+" min eleman: "+nums.Min());
+
+            //string[] subjects = new string[4];
+            //for (int i = 0; i < subjects.Length; i++)
+            //{
+            //    Console.WriteLine(i+1+". elemanı giriniz: ");
+            //    subjects[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine("--------------------------");
+            //for (int i = 0;i < subjects.Length;i++)
+            //{
+            //    Console.WriteLine(subjects[i]);
+            //}
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            //int sum = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+            Console.Read();
+        }
+    }
+}
