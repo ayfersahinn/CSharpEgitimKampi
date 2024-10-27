9b25a2f4f216f486937559c33f4ad73cd46651e4
Ayfer Şahin <ayfers547@gmail.com>
2024-10-27 20:11:21 +0300

    listed products

--- 10_task10/Program.cs
+++ 10_task10/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,11 @@ namespace _10_task10
         static void Main(string[] args)
         {
             SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-5ANGUIR\\SQLEXPRESS02;Initial Catalog=egitimKampi;Integrated Security=True;Encrypt=False");
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
             #region adding category 
-            //Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
-            //Console.WriteLine();
-            //Console.WriteLine("--------------------------------------------");
+
             //Console.Write("Eklemek istediğiniz kategori adı: ");
             //string categoryName = Console.ReadLine();
 
@@ -27,20 +29,37 @@ namespace _10_task10
             //sqlConnection.Close();
             #endregion
             #region adding product
-            string productName;
-            decimal productPrice;
-            bool productStatus;
-            Console.Write("Ürün adı: ");
-            productName = Console.ReadLine();
-            Console.Write("Ürün fiyatı: ");
-            productPrice = decimal.Parse(Console.ReadLine());
+            //string productName;
+            //decimal productPrice;
+            //bool productStatus;
+            //Console.Write("Ürün adı: ");
+            //productName = Console.ReadLine();
+            //Console.Write("Ürün fiyatı: ");
+            //productPrice = decimal.Parse(Console.ReadLine());
+            //sqlConnection.Open();
+            //SqlCommand cmd = new SqlCommand("insert into products (ProductName, ProductPrice, ProductStatus) values (@p1, @p2, @p3)",sqlConnection);
+            //cmd.Parameters.AddWithValue("@p1", productName);
+            //cmd.Parameters.AddWithValue("@p2", productPrice);
+            //cmd.Parameters.AddWithValue("@p3", true);
+            //cmd.ExecuteNonQuery();
+            //Console.WriteLine("Ürün başarıyla eklendi");
+            //sqlConnection.Close();
+            #endregion
+
+            #region listing products
             sqlConnection.Open();
-            SqlCommand cmd = new SqlCommand("insert into products (ProductName, ProductPrice, ProductStatus) values (@p1, @p2, @p3)",sqlConnection);
-            cmd.Parameters.AddWithValue("@p1", productName);
-            cmd.Parameters.AddWithValue("@p2", productPrice);
-            cmd.Parameters.AddWithValue("@p3", true);
-            cmd.ExecuteNonQuery();
-            Console.WriteLine("Ürün başarıyla eklendi");
+            SqlCommand cmd = new SqlCommand("select *from products", sqlConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                foreach(var item in dr.ItemArray)
+                {
+                    Console.Write(item.ToString() + " ");
+                }
+                Console.WriteLine();
+            }
             sqlConnection.Close();
             #endregion
             Console.Read();
