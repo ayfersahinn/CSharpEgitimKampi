8e54fa560ddc3f881f48bae3b0fc8072a7fc2b2d
Ayfer Şahin <ayfers547@gmail.com>
2024-10-27 19:50:10 +0300

    category name is added to database

---
+++ 10_task10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.Write("Eklemek istediğiniz kategori adı: ");
+            string categoryName = Console.ReadLine();
+            SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-5ANGUIR\\SQLEXPRESS02;Initial Catalog=egitimKampi;Integrated Security=True;Encrypt=False");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("insert into categories (CategoryName) values (@p1)",sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@p1",categoryName);
+            sqlCommand.ExecuteNonQuery();
+            Console.WriteLine("Kategori başarıyla eklendi");
+            sqlConnection.Close();
+
+            Console.Read();
+        }
+    }
+}
